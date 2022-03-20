e0e5cf72da6b10b42a327ed1d87533cfac53a28f
virtualmonkey <luis212urbinau@gmail.com>
2022-03-19 20:42:35 -0600

    Added sequential code and implementation in main

--- main.cpp
+++ main.cpp
@@ -4,20 +4,24 @@
 #include <iomanip>
 using namespace std::chrono;
 
-int numero_threads = 2000;
+// Valores iniciales de prueba
+int numero_threads = 5000;
 double Tl = 0.0;
 double Tr = 80.0;
 
 int main(int argc, char const *argv[])
 {
-    numero_threads = strtol(argv[1], NULL, 10);
-    Tl = strtol(argv[2], NULL, 10);
-    Tr = strtol(argv[3], NULL, 10);
+    if (argc >= 3){
+        numero_threads = strtol(argv[1], NULL, 10);
+        Tl = strtol(argv[2], NULL, 10);
+        Tr = strtol(argv[3], NULL, 10);
+    }
 
     double C=0.35;
+
     int intervalosX = numero_threads;
     int intervalosTiempo = int((pow(intervalosX, 2) * 6e-3)/C);
-
+    
     std::cout << "Corriendo programa con parámetros: " << std::endl;
 
     std::cout << "Intervalos de distancia = " << intervalosX << std::endl;
@@ -32,9 +36,9 @@ int main(int argc, char const *argv[])
     
     auto finalSecuencial = high_resolution_clock::now();
 
-    auto tiempoSecuencial = duration_cast<microseconds>(finalSecuencial - inicialSecuencial);
+    auto tiempoSecuencial = duration_cast<milliseconds>(finalSecuencial - inicialSecuencial);
 
-    std::cout << "Tiempo secuencial: " << tiempoSecuencial.count() << " μs" << std::endl;
+    std::cout << "Tiempo secuencial: " << tiempoSecuencial.count() << " ms" << std::endl;
 
     return 0;
 }
